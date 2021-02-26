8b86e0862cdec9b5099398267db71c3d0d4c8736
Chris Cannam <cannam@all-day-breakfast.com>
2021-02-26 11:43:21 +0000

    Add include path

--- otherbuilds/check.sh
+++ otherbuilds/check.sh
@@ -2,11 +2,11 @@
 set -eu
 if [ ! -d /Applications ]; then
     make -f otherbuilds/Makefile.linux
-    g++ main/main.cpp lib/librubberband.a -Isrc -o test -lsndfile -lsamplerate -lpthread
+    g++ main/main.cpp lib/librubberband.a -I. -Isrc -o test -lsndfile -lsamplerate -lpthread
     ./test -V
 else
     make -f otherbuilds/Makefile.macos
-    c++ main/main.cpp lib/librubberband.a -Isrc -o test -lsndfile -lsamplerate -framework Accelerate
+    c++ main/main.cpp lib/librubberband.a -I. -Isrc -o test -lsndfile -lsamplerate -framework Accelerate
     ./test -V
     make -f otherbuilds/Makefile.macos clean
     make -f otherbuilds/Makefile.ios
