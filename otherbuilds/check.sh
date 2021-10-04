829a208fd024ded153c1e2231a98bad2101f6b9a
Chris Cannam <cannam@all-day-breakfast.com>
2021-10-04 12:24:59 +0100

    Check single-compilation-unit build too

--- otherbuilds/check.sh
+++ otherbuilds/check.sh
@@ -4,10 +4,14 @@ if [ ! -d /Applications ]; then
     make -f otherbuilds/Makefile.linux
     g++ main/main.cpp lib/librubberband.a -I. -Isrc -o test -lsndfile -lsamplerate -lpthread
     ./test -V
+    g++ main/main.cpp single/RubberBandSingle.cpp -o test_single -lsndfile
+    ./test_single -V
 else
     make -f otherbuilds/Makefile.macos
     c++ main/main.cpp lib/librubberband.a -I. -Isrc -o test -lsndfile -lsamplerate -framework Accelerate
     ./test -V
     make -f otherbuilds/Makefile.macos clean
     make -f otherbuilds/Makefile.ios
+    c++ main/main.cpp single/RubberBandSingle.cpp -o test_single -lsndfile
+    ./test_single -V
 fi
