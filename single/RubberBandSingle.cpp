515f5e2bd1455efd309c2efa4d6bef359c348a42
Chris Cannam <cannam@all-day-breakfast.com>
2022-08-18 12:03:29 +0100

    Allow pre-configuration if explicitly specified

--- single/RubberBandSingle.cpp
+++ single/RubberBandSingle.cpp
@@ -44,6 +44,8 @@
     standalone library.
 */
 
+#ifndef ALREADY_CONFIGURED
+
 #define USE_BQRESAMPLER 1
 
 #define NO_TIMING 1
@@ -54,6 +56,8 @@
 #define HAVE_VDSP 1
 #else
 #define USE_BUILTIN_FFT 1
+#endif
+
 #endif
 
 #include "../src/faster/AudioCurveCalculator.cpp"
