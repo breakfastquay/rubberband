52fc576500931afb5a7374a241c5f386b14a5e29
Chris Cannam <cannam@all-day-breakfast.com>
2022-09-30 10:36:35 +0100

    Remove obsolete file

--- src/temporary.cpp
+++
@@ -1,8 +0,0 @@
-#include "finer/R3StretcherImpl.h"
-
-int main(int argc, char **argv)
-{
-    RubberBand::R3StretcherImpl::Parameters parameters(44100.0, 2);
-    RubberBand::R3StretcherImpl impl(parameters);
-}
-
